2932a028b88ae18bb353cf63735eacd081ed0814
buckleader <jasmin28virdi@gmail.com>
2021-01-05 23:11:31 +0530

    remove logs from api.js

--- src/api.js
+++ src/api.js
@@ -1,12 +1,8 @@
-import config from 'config';
-import { slack } from "../config/index"
 const axios = require('axios');
 const qs = require('querystring');
-const slackConfig = config.get('slack');
 const apiUrl = 'https://slack.com/api';
 
 const callAPIMethod = async (method, payload, token) => {
-    console.log("callAPIMethod", method, payload, token)
     let data = Object.assign({ token }, payload);
     let result = await axios.post(`${apiUrl}/${method}`, qs.stringify(data));
     return result.data;
